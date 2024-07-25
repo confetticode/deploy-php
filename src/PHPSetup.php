5f57760106986a7aa7504fa3e635e046a6fa5acc
Quynh Nguyen <seriquynh@gmail.com>
2024-07-25 16:11:27 +0700

    WIP

---
+++ src/PHPSetup.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SendCode\DeployPHP;
+
+use Symfony\Component\Process\Process;
+
+class PHPSetup implements ScriptInterface
+{
+    public function __construct(private readonly Server $server)
+    {
+    }
+
+    public function run(): void
+    {
+        $user = $this->server->user;
+        $host = $this->server->host;
+        $port = $this->server->port;
+        $priKey = $this->server->priKey;
+
+        $script = 'php-setup.sh';
+        $file = __DIR__ . '/../scripts/' . $script;
+        $code = file_get_contents($file);
+
+        file_put_contents($scriptPath = __DIR__ . '/' . $script, $code);
+
+        $process = Process::fromShellCommandline(
+            "ssh -o StrictHostKeyChecking=no -i $priKey -p $port $user@$host 'sudo bash -s' < $scriptPath"
+        );
+
+        $process->setTimeout(null);
+
+        $process->run(function ($type, $buffer) {
+            // file_put_contents('log.txt', $buffer, FILE_APPEND);
+            echo $buffer;
+        });
+
+        @unlink($scriptPath);
+    }
+}
